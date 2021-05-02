e323f1c870c96a09411de5f64807c37f02ca08c6
Саляхов Сергей <salyakhovsergei@gmail.com>
2021-05-03 07:26:30 +1000

    Добавьте файлы проекта.

---
+++ Task9.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Surnames srn = new Surnames();
+           
+            srn.MyEvent += srn.SortSurNames;
+
+            Console.WriteLine("Введите 5 фамилий для сотрировки");
+
+            List<string> lastNames1 = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                lastNames1.Add(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Если необходимо отсортировать от А до Я введите 1");
+            Console.WriteLine("Если необходимо отсортировать от Я до А введите 2");
+
+            try
+            {
+                srn.SortSurNames(lastNames1);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task9.2/Surnames.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9._2
+{
+    public class Surnames
+    {
+        public delegate void myDelegate(List<string> list);
+        public event myDelegate MyEvent;
+
+        Exception myException = new Exception("Неверный ввод");
+
+        public void SortSurNames(List <string> lastNames)
+        {
+            byte i = Convert.ToByte(Console.ReadLine());
+
+            if (i != 1 && i != 2) throw myException;
+
+            switch (i)
+            {
+                case 1:
+                    lastNames.Sort();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Отсортированный список от А до Я");
+
+                    foreach (string s in lastNames)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    break;
+
+                case 2:
+                    lastNames.Sort();
+                    lastNames.Reverse();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Отсортированный список от Я до А");
+
+                    foreach (string s in lastNames)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    break;
+            }
+
+            SurnamesCheck(lastNames);
+        }
+        protected virtual void SurnamesCheck(List<string> list)
+        {
+            MyEvent?.Invoke(list);
+        }
+    }
+}
